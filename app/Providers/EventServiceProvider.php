fd64a87b4339c545a4ea9783ea3e215ed6f948a8
chenzhulin <chenzhulin123456@gmail.com>
2019-03-19 03:40:33 +0000

    认证后的消息提醒

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,7 +17,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
+   	 ],
+	\Illuminate\Auth\Events\Verified::class =>[
+	     \App\Listeners\EmailVerified::class,
+	],
     ];
 
     /**
