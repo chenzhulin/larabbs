ea779d4316ded672bb901ed61e646cafa9e9c754
chenzhulin <chenzhulin123456@gmail.com>
2019-03-19 08:46:38 +0000

    上传图片

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
+
 class UsersController extends Controller
 {
 	public function show(User $user)
@@ -20,8 +22,21 @@ class UsersController extends Controller
 		
 	}
 
-	public function update(User $user,UserRequest  $request)
+	public function update(User $user,UserRequest  $request,ImageUploadHandler $uploader)
 	{
+		$data = $request->all();
+
+		if($request->avatar){
+
+            $result = $uploader->save($request->avatar,'avatars',$user->id);
+
+			if($result){
+				$data['avatar'] = $result['path'];
+			
+			}
+		
+		}
+
 		$user->update($request->all());
 		
 		return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
