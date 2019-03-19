21e69094d6db737875b9a1ff36a3f8947f1c6e79
chenzhulin <chenzhulin123456@gmail.com>
2019-03-19 09:05:06 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -27,6 +27,7 @@ class UserRequest extends FormRequest
 		'name' =>'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'.Auth::id(),
 		'email'=>'required|email',
 		'introduction' =>'max:80',
+		'avatar' =>'mimes:jpeg,bmp.png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -34,11 +35,18 @@ class UserRequest extends FormRequest
     {
     
 	    return [
+		    'avatar.mimes'=>'头像必须是jpeg,bmp,png,gif 格式的图片',
 
-		'name.unique' =>'用户名已被占用，请重新输入',
-		'name.regex'  =>'用户名只支持英文、数字、横杠和下划线',
-		'name.between'=>'用户名必须介于3-25个字符之间',
-		'name.required'=>'用户名不能为空',
+		    'avatar.dimensions'=>'图像的情绪度不够,宽和高需要 208px 以上',
+
+		    'name.unique' =>'用户名已被占用，请重新输入',
+
+		    'name.regex'  =>'用户名只支持英文、数字、横杠和下划线',
+
+		    'name.between'=>'用户名必须介于3-25个字符之间',
+
+		    'name.required'=>'用户名不能为空',
+
 	
 	
 	];
