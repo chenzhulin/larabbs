27a0821eb60a8f21f61f83f60301ec25e2868911
chenzhulin <chenzhulin123456@gmail.com>
2019-03-20 02:47:13 +0000

    填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    $faker = app(Faker\Generator::class);
+
+	    $avatars = [
+	    	
+		    'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png',
+
+		    'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png',
+
+		    'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png',
+
+		    'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png',
+
+		    'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+
+		    'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png',  
+	    
+	    ];
+
+	    $users = factory(User::class)
+		    	->times(10)
+			->make()
+			->each(function ($user,$index) use($faker,$avatars)
+			{
+			
+				$user->avatar = $faker->randomElement($avatars);
+			
+			
+			});
+
+	    $user_array = $users->makeVisible(['password','remember_token'])->toArray();
+
+	    User::insert($user_array);
+
+	    $user = User::find(1);
+
+	    $user->name = 'Summer';
+
+	    $user->email = 'summer@example.com';
+
+	    $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+
+	    $user->save();
+    }
+}
