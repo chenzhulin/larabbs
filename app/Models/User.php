ea5c68e44d55b9c30780ae8148566e85b8227aea
chenzhulin <chenzhulin123456@gmail.com>
2019-03-20 07:45:52 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,5 +36,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+	];
+
+	
+    public function topics()
+    {
+
+	    return $this->hasMany(Topic::class);
+    }
 }
