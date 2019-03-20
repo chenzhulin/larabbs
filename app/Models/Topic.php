65abfdb62b0b5cfdc679d1eb1db96eb922bb0a3b
chenzhulin <chenzhulin123456@gmail.com>
2019-03-20 07:11:44 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -26,6 +26,36 @@ class Topic extends Model
 	
 	}
 
+	public function scopeWithOrder($query,$order)
+	{
 
+		switch($order){
+		
+			case 'recent':
+				$query->recent();
+				break;
+
+
+			default:
+				$query->recentReplied();
+				break;	
+		}
+
+		return $query->with('user','category');
+	
+	}
+
+
+	public function scopeRecentReplied($query)
+	{
+		return $query->orderBy('updated_at','desc');
+	
+	}
+
+	public function scopeRecent($query)
+	{
+		return $query->orderBy('created_at','desc');
+	
+	}
 
 }
--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -10,14 +10,22 @@
 	 {{ $category->name }} : {{ $category->description }}
 	</div>
 	@endif
-	<div class="card">
-	 <div class="card-header bg-transparent">
-	   <ul class="nav nav-pills">
-		<li class="nav-item"><a class="nav-link active" href="#"> 最后回复</a> </li>
-		<li class="nav-item"><a class="nav-link" href="#">最新发布</a></li>
-	   </ul>
-	 </div>
-	
+<div class="card ">
+      <div class="card-header bg-transparent">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a class="nav-link {{ active_class( ! if_query('order', 'recent')) }}" href="{{ Request::url() }}?order=default">
+              最后回复
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ active_class(if_query('order', 'recent')) }}" href="{{ Request::url() }}?order=recent">
+              最新发布
+            </a>
+          </li>
+        </ul>
+      </div>
+
  	<div class="card-body">
  	  {{-- 话题列表 --}}
 		@include('topics._topic_list',['topics' =>$topics])
