9bc0d27e87f62e1e440f1c27953781d0ae5c1b99
chenzhulin <chenzhulin123456@gmail.com>
2019-03-21 01:49:10 +0000

    用户可以删除自己发的话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -63,7 +63,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功！');
 	}
 
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,14 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
 	    return $this->hasMany(Topic::class);
     }
+
+
+    public function isAuthorOf($model)
+    {
+    
+    	return $this->id == $model->user_id;
+    
+    }
+
+
 }
