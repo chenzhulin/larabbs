efd5b59a0df0c672d5769e6c64350d6dd11fc4b9
chenzhulin <chenzhulin123456@gmail.com>
2019-03-21 01:35:37 +0000

    用户可以编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -63,14 +63,17 @@ $(document).ready(function() {
                 <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required />
               </div>
 
+
               <div class="form-group">
                 <select class="form-control" name="category_id" required>
-                  <option value="" hidden disabled selected>请选择分类</option>
+                  <option value="" hidden disabled {{$topic->id ? '':'selected'}}>请选择分类</option>
                   @foreach ($categories as $value)
-                  <option value="{{ $value->id }}">{{ $value->name }}</option>
+		  <option value="{{ $value->id }}" {{$topic->category_id== $value->id ? 'selected' : ''}}>{{ $value->name }}
+		</option>
                   @endforeach
                 </select>
               </div>
+
 
               <div class="form-group">
                 <textarea name="body" class="form-control" id="editor" rows="6" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
