d0a6a89c2e0309fc9666c74175635713623451f8
chenzhulin <chenzhulin123456@gmail.com>
2019-03-21 08:44:21 +0000

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,33 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
-
+use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-	use Notifiable, MustVerifyEmailTrait;
+	use MustVerifyEmailTrait;
+
+	use Notifiable{
+	
+		notify as protected laravelNotify;
+	}
+
+	public function notify($instance)
+	{
+		if($this->id == Auth::id()){
+		
+			return;
+		}
+
+
+		if(method_exists($instance,'toDatabase')){
+		
+			$this->increment('notification_count');
+		}
+
+		$this->laravelNotify($instance);
+	
+	}
     /**
      * The attributes that are mass assignable.
      *
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
+use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -17,7 +18,9 @@ class ReplyObserver
     public function created(Reply $reply)
     {
     	$reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+	$reply->topic->save();
+
+	$reply->topic->user->notify(new TopicReplied($reply));
    	 
     }
 
