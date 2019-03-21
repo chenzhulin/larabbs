af4ae64c99855e664c807f4a6f531328a7858944
chenzhulin <chenzhulin123456@gmail.com>
2019-03-21 09:12:09 +0000

    消息通知列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -19,22 +19,31 @@
       </ul>
 
       <!-- Right Side Of Navbar -->
+
       <ul class="navbar-nav navbar-right">
-        <!-- Authentication Links -->
+
+
+ <!-- Authentication Links -->
         @guest
           <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
           <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
-	  @else
-	    <li class="nav-item">	  
-	    <a class="nav-link mt-2 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
-		<i class="fa fa-plus"></i>
-	    </a>
-	   </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
-              {{ Auth::user()->name }}
+        @else
+          <li class="nav-item">
+            <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+              <i class="fa fa-plus"></i>
             </a>
+          </li>
+          <li class="nav-item notification-badge">
+            <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
+              {{ Auth::user()->notification_count }}
+            </a>
+          </li>
+
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                      {{ Auth::user()->name }}
+                  </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 	       <a class="dropdown-item" href="{{ route('users.show',Auth::id())}}">
 		<i class="far fa-user mr-2"></i>
