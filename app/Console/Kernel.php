6bea2b86b08beb2f576c64a74a0a608bede321fe
chenzhulin <chenzhulin123456@gmail.com>
2019-03-22 08:57:33 +0000

    活跃用户

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+            // $schedule->command('inspire')
+	    //          ->hourly();
+
+	    $schedule->command('larabbs:calculate-active-user')->hourly();
+
     }
 
     /**
--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,15 +8,21 @@ use App\Models\Topic;
 
 use App\Models\Category;
 
+use App\Models\User;
+
 class CategoriesController extends Controller
 {
-	public function show(Category $category,Request $request,Topic $topic)
+	public function show(Category $category,Request $request,Topic $topic,User $user)
 	{
 		$topics = $topic->withOrder($request->order)
 				->where('category_id',$category->id)
 				->paginate(20);
 
-		return view('topics.index',compact('topics','category'));
+
+		$active_users = $user->getActiveUsers();
+
+
+		return view('topics.index',compact('topics','category','active_users'));
 	
 	}
 }
