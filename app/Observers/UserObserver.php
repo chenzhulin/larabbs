c466273ea945192776d84c83f02e934c53a39e80
chenzhulin <chenzhulin123456@gmail.com>
2019-03-25 02:41:13 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,6 +9,19 @@ use App\Models\User;
 
 class UserObserver
 {
+
+	
+    public function saving(User $user)
+    {
+    
+    	if(empty($user->avatar)){
+		$user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+	
+	
+	}
+    
+    
+    }
     public function creating(User $user)
     {
         //
